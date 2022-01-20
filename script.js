1df974fccceba2932f252a9a2f627b9fb91f4469
ADITYA <74747340+DARKGHOST13578@users.noreply.github.com>
2022-01-20 10:08:34 +0530

    WEBTASK
    removed bugs
    shifted code to html

--- script.js
+++ script.js
@@ -7,46 +7,6 @@ setInterval(bgi, 100);
 
 
 
-    //default,granted,denied
-  console.log(Notification.permission);
-
-  if (Notification.permission === "granted") {
-        notify();
-  }
-
-  else if (Notification.permission !== "denied") {
-    Notification.requestPermission().then(permission => {
-             if (Notification.permission === "granted") {
-                   notify();
-      }
-  });
-};
-
-    document.addEventListener("DOMContentLoaded", function(event) {
-       document.getElementById('id01').style.display = "block"
-    document.getElementById('bod').style.filter = "blur(15px)"  });
-
-
-
-
-
-
-//REMEMBER ME
-
-  const rmCheck = document.getElementById("rememberMe");
-    var emailInput = document.getElementById("user");
-    var psInput = document.getElementById("pd");
-
-if (localStorage.checkbox && localStorage.checkbox !== "") {
-  rmCheck.setAttribute("checked", "checked");
-  emailInput.value = localStorage.username;
-  psInput.value = localStorage.password;
-} else {
-  rmCheck.removeAttribute("checked");
-  emailInput.value = "";
-}
-
-
 
 
 
@@ -161,28 +121,32 @@ document.oncontextmenu = function(e)
    if (dayOfWeekNumber == 0) {
     document.getElementById("all").innerHTML = ""
     document.getElementById("classes").href = ""
-    document.getElementById("table1").style.display = "none"
-    document.getElementById("table2").style.display = "none"
+    document.getElementById("ID").innerHTML = ""
+    document.getElementById("PW").innerHTML = ""
    } 
-   else {
+
+   else{
 
 //TUTION 1
-   if (hours == 5 && minutes > 30) {
-    document.getElementById("all").innerHTML = "TUTION CLASS TIMINGS 5:30 AM TO 7:00 AM"
+           if (hours == 5 && minutes > 30) {
+    document.getElementById("all").innerHTML = "TUTION STUDY HOURS TIMINGS 5:30 AM TO 7:00 AM"
     document.getElementById("classes").href = "https://us02web.zoom.us/j/3883317582?pwd=QmNRNk5FU0l0VkwxaWVPbnFaVi9PZz09"
-    document.getElementById("table2").style.display = "none"
+    document.getElementById("ID").innerHTML = "THE MEETIN ID IS"
+    document.getElementById("PW").innerHTML = "THE MEETING PASSWORD IS"
   }
 
    else if (hours == 6) {
-    document.getElementById("all").innerHTML = "TUTION CLASS TIMINGS 5:30 AM TO 7:00 AM"
+    document.getElementById("all").innerHTML = "TUTION STUDY HOURS TIMINGS 5:30 AM TO 7:00 AM"
     document.getElementById("classes").href = "https://us02web.zoom.us/j/3883317582?pwd=QmNRNk5FU0l0VkwxaWVPbnFaVi9PZz09"
-    document.getElementById("table2").style.display = "none"
+    document.getElementById("ID").innerHTML = "THE MEETIN ID IS"
+    document.getElementById("PW").innerHTML = "THE MEETING PASSWORD IS"
   }
 
   else if (hours == 7 && minutes < 30) {
-    document.getElementById("all").innerHTML = "TUTION CLASS TIMINGS 5:30 AM TO 7:00 AM"
+    document.getElementById("all").innerHTML = "TUTION STUDY HOURS TIMINGS 5:30 AM TO 7:00 AM"
     document.getElementById("classes").href = "https://us02web.zoom.us/j/3883317582?pwd=QmNRNk5FU0l0VkwxaWVPbnFaVi9PZz09"   
-    document.getElementById("table2").style.display = "none"
+    document.getElementById("ID").innerHTML = "THE MEETIN ID IS"
+    document.getElementById("PW").innerHTML = "THE MEETING PASSWORD IS"
   }
 
 
@@ -191,8 +155,6 @@ document.oncontextmenu = function(e)
            else if (dayOfWeekNumber > 0 && dayOfWeekNumber < 4 && hours == 9 && minutes < 41) {
     document.getElementById("all").innerHTML = "ENGLISH CLASS JEEVAN SIR 9:00 A TO 9:40AM"
     document.getElementById("classes").href = "https://us05web.zoom.us/j/9569214251?pwd=MVNwNkRhWElPTmxpazBkUVZuZ2lpZz09#success"
-    document.getElementById("table2").style.display = "none"
-    document.getElementById("table1").style.display = "none"
     document.getElementById("ID").innerHTML = "THE MEETIN ID IS"
     document.getElementById("PW").innerHTML = "THE MEETING PASSWORD IS"
    } 
@@ -200,8 +162,6 @@ document.oncontextmenu = function(e)
    else if (dayOfWeekNumber > 3 && dayOfWeekNumber < 7 && hours == 9 && minutes < 41) {
     document.getElementById("all").innerHTML = "SOCIAL CLASS FARZANA MAM 09:00 AM TO 09:40 AM"
     document.getElementById("classes").href = "https://us05web.zoom.us/j/2022786585?pwd=WHRPaGFKMGpjN09XaWpkV0NST2Qvdz09#success"
-    document.getElementById("table2").style.display = "none"
-    document.getElementById("table1").style.display = "none"
     document.getElementById("ID").innerHTML = "THE MEETIN ID IS"
     document.getElementById("PW").innerHTML = "THE MEETING PASSWORD IS"
    }
@@ -209,8 +169,6 @@ document.oncontextmenu = function(e)
    else if (hours == 9 && minutes > 49) {
     document.getElementById("all").innerHTML = "MATHS CLASS FAREED SIR 09:50 AM TO 10:30 AM"
     document.getElementById("classes").href = "https://us05web.zoom.us/j/7706149246?pwd=YS96dDd3b1MrK2pOSTFmc0h2WVdwZz09#success"
-    document.getElementById("table2").style.display = "none"
-    document.getElementById("table1").style.display = "none"
     document.getElementById("ID").innerHTML = "THE MEETIN ID IS"
     document.getElementById("PW").innerHTML = "THE MEETING PASSWORD IS"
    }
@@ -218,8 +176,6 @@ document.oncontextmenu = function(e)
    else if (hours == 10 && minutes < 31) {
     document.getElementById("all").innerHTML = "MATHS CLASS FAREED SIR 09:50 AM TO 10:30 AM"
     document.getElementById("classes").href = "https://us05web.zoom.us/j/7706149246?pwd=YS96dDd3b1MrK2pOSTFmc0h2WVdwZz09#success"
-    document.getElementById("table2").style.display = "none"
-    document.getElementById("table1").style.display = "none"
     document.getElementById("ID").innerHTML = "THE MEETIN ID IS"
     document.getElementById("PW").innerHTML = "THE MEETING PASSWORD IS"
    }
@@ -227,8 +183,6 @@ document.oncontextmenu = function(e)
    else if (hours == 10 && minutes > 39) {
     document.getElementById("all").innerHTML = "SCIENCE CLASS SWETHA MAM 10:40 AM TO 11:20 AM"
     document.getElementById("classes").href = "https://us05web.zoom.us/j/2251852346?pwd=cGY3NUUxWHREYWl4MjlJZTlobGtHZz09#success"
-    document.getElementById("table2").style.display = "none"
-    document.getElementById("table1").style.display = "none"
     document.getElementById("ID").innerHTML = "THE MEETIN ID IS = 225 185 2346"
     document.getElementById("PW").innerHTML = "THE MEETING PASSWORD IS = sciswe23"
    }
@@ -236,8 +190,6 @@ document.oncontextmenu = function(e)
    else if (hours == 11 && minutes < 21) {
     document.getElementById("all").innerHTML = "SCIENCE CLASS SWETHA MAM 10:40 AM TO 11:20 AM"
     document.getElementById("classes").href = "https://us05web.zoom.us/j/2251852346?pwd=cGY3NUUxWHREYWl4MjlJZTlobGtHZz09#success"
-    document.getElementById("table2").style.display = "none"
-    document.getElementById("table1").style.display = "none"
     document.getElementById("ID").innerHTML = "THE MEETIN ID IS = 225 185 2346"
     document.getElementById("PW").innerHTML = "THE MEETING PASSWORD IS = sciswe23"
    }
@@ -245,8 +197,6 @@ document.oncontextmenu = function(e)
    else if (dayOfWeekNumber > 0 && dayOfWeekNumber < 4 && hours == 11 && minutes > 29) {
     document.getElementById("all").innerHTML = "हिंदी HINDI  CLASS  BASHEER  SIR 11:30 AM TO 12:10 PM"
     document.getElementById("classes").href = "https://us05web.zoom.us/j/4812910424?pwd=RGhST3RtZGYyR2pxNStodzM5STVDQT09#success"
-    document.getElementById("table2").style.display = "none"
-    document.getElementById("table1").style.display = "none"
     document.getElementById("ID").innerHTML = "THE MEETIN ID IS"
     document.getElementById("PW").innerHTML = "THE MEETING PASSWORD IS"
    }
@@ -254,8 +204,6 @@ document.oncontextmenu = function(e)
    else if (dayOfWeekNumber > 0 && dayOfWeekNumber < 4 && hours == 12 && minutes < 11) {
     document.getElementById("all").innerHTML = "हिंदी HINDI  CLASS  BASHEER  SIR 11:30 AM TO 12:10 PM"
     document.getElementById("classes").href = "https://us05web.zoom.us/j/4812910424?pwd=RGhST3RtZGYyR2pxNStodzM5STVDQT09#success"
-    document.getElementById("table2").style.display = "none"
-    document.getElementById("table1").style.display = "none"
     document.getElementById("ID").innerHTML = "THE MEETIN ID IS"
     document.getElementById("PW").innerHTML = "THE MEETING PASSWORD IS"
    }
@@ -263,8 +211,6 @@ document.oncontextmenu = function(e)
    else if (dayOfWeekNumber > 3 && dayOfWeekNumber < 7 && hours == 11 && minutes > 29) {
     document.getElementById("all").innerHTML = "తెలుగు TELUGU CLASS SHUBHA RANI MAM 11:30 AM TO 12:10 PM"
     document.getElementById("classes").href = "https://us05web.zoom.us/j/9257716263?pwd=UFgyRFRXM3B0d0t1b05mUkRUb3J0UT09#success"
-    document.getElementById("table2").style.display = "none"
-    document.getElementById("table1").style.display = "none"
     document.getElementById("ID").innerHTML = "THE MEETIN ID IS"
     document.getElementById("PW").innerHTML = "THE MEETING PASSWORD IS"
    }
@@ -272,8 +218,6 @@ document.oncontextmenu = function(e)
    else if (dayOfWeekNumber > 3 && dayOfWeekNumber < 7 && hours == 12 && minutes > 11) {
     document.getElementById("all").innerHTML = "తెలుగు TELUGU CLASS SHUBHA RANI MAM 11:30 AM TO 12:10 PM"
     document.getElementById("classes").href = "https://us05web.zoom.us/j/9257716263?pwd=UFgyRFRXM3B0d0t1b05mUkRUb3J0UT09#success"
-    document.getElementById("table2").style.display = "none"
-    document.getElementById("table1").style.display = "none"
     document.getElementById("ID").innerHTML = "THE MEETIN ID IS"
     document.getElementById("PW").innerHTML = "THE MEETING PASSWORD IS"
    }
@@ -281,8 +225,6 @@ document.oncontextmenu = function(e)
    else if (hours == 13 && minutes > 29) {
     document.getElementById("all").innerHTML = "IIT PHYSICS CLASS PRADEEP SIR 01:30 PM TO 02:10 PM"
     document.getElementById("classes").href = "https://us04web.zoom.us/j/8497371316?pwd=UTdRQUlDNEZkS0J5WUhMcmprSzhMQT09#success"  
-    document.getElementById("table2").style.display = "none"
-    document.getElementById("table1").style.display = "none"
     document.getElementById("ID").innerHTML = "THE MEETIN ID IS"
     document.getElementById("PW").innerHTML = "THE MEETING PASSWORD IS"
    }
@@ -290,8 +232,6 @@ document.oncontextmenu = function(e)
    else if (hours == 14 && minutes < 11) {
     document.getElementById("all").innerHTML = "IIT PHYSICS CLASS PRADEEP SIR 01:30 PM TO 02:10 PM"
     document.getElementById("classes").href = "https://us04web.zoom.us/j/8497371316?pwd=UTdRQUlDNEZkS0J5WUhMcmprSzhMQT09#success"  
-    document.getElementById("table2").style.display = "none"
-    document.getElementById("table1").style.display = "none"
     document.getElementById("ID").innerHTML = "THE MEETIN ID IS"
     document.getElementById("PW").innerHTML = "THE MEETING PASSWORD IS"
    }
@@ -299,8 +239,6 @@ document.oncontextmenu = function(e)
    else if (hours == 14 && minutes > 19) {
     document.getElementById("all").innerHTML = "IIT CHEMISTRY CLASS LAVANYA MAM 02:20 PM TO 03:00 PM"
     document.getElementById("classes").href = "https://us05web.zoom.us/j/6844417292?pwd=c29qNlE5eHBPcFdsRGFkTnluNXA3QT09#success"
-    document.getElementById("table2").style.display = "none"
-    document.getElementById("table1").style.display = "none"
     document.getElementById("ID").innerHTML = "THE MEETIN ID IS"
     document.getElementById("PW").innerHTML = "THE MEETING PASSWORD IS"
    }
@@ -308,31 +246,33 @@ document.oncontextmenu = function(e)
    else if (hours == 15 && minutes < 9 && minutes > 51) {
     document.getElementById("all").innerHTML = "IIT MATHS CLASS NAGTEJ SIR 03:10 PM TO 03:50 PM"
     document.getElementById("classes").href = "https://us05web.zoom.us/j/9685212865?pwd=dFlQRDhKL2N6K2JQSmRxM0NRTDAwdz09#success"
-    document.getElementById("table2").style.display = "none"
-    document.getElementById("table1").style.display = "none"
     document.getElementById("ID").innerHTML = "THE MEETIN ID IS"
     document.getElementById("PW").innerHTML = "THE MEETING PASSWORD IS"
    }
-}
+
 
 //TUTION 2
-           if (hours == 18 ) {
+           else if (hours == 18 ) {
     document.getElementById("all").innerHTML = "TUTION CLASS TIMINGS 6:00 PM TO 8:30 PM"
     document.getElementById("classes").href = "https://us02web.zoom.us/j/3883317582?pwd=QmNRNk5FU0l0VkwxaWVPbnFaVi9PZz09"
-    document.getElementById("table1").style.display = "none"
+    document.getElementById("ID").innerHTML = "THE MEETIN ID IS"
+    document.getElementById("PW").innerHTML = "THE MEETING PASSWORD IS"
   }
   
    else if (hours == 19) {
     document.getElementById("all").innerHTML = "TUTION CLASS TIMINGS 6:00 PM TO 8:30 PM"
     document.getElementById("classes").href = "https://us02web.zoom.us/j/3883317582?pwd=QmNRNk5FU0l0VkwxaWVPbnFaVi9PZz09"
-    document.getElementById("table1").style.display = "none"
+    document.getElementById("ID").innerHTML = "THE MEETIN ID IS"
+    document.getElementById("PW").innerHTML = "THE MEETING PASSWORD IS"
   }
 
    else if (hours == 20 && minutes < 31) {
     document.getElementById("all").innerHTML = "TUTION CLASS TIMINGS 6:00 PM TO 8:30 PM"
     document.getElementById("classes").href = "https://us02web.zoom.us/j/3883317582?pwd=QmNRNk5FU0l0VkwxaWVPbnFaVi9PZz09"
-    document.getElementById("table1").style.display = "none"
+    document.getElementById("ID").innerHTML = "THE MEETIN ID IS"
+    document.getElementById("PW").innerHTML = "THE MEETING PASSWORD IS"
   }
+
 
 
 
@@ -342,13 +282,12 @@ document.oncontextmenu = function(e)
     document.getElementById("all").innerHTML = ""
     document.getElementById("classes").href = ""
     document.getElementById("no").innerHTML = "NO CLASS IS STREAMING NOW"
-    document.getElementById("table1").style.display = "none"
-    document.getElementById("table2").style.display = "none"
+    document.getElementById("ID").innerHTML = ""
+    document.getElementById("PW").innerHTML = ""
       }
-
+}
 
 //WEEKS
- }
    if (dayOfWeekNumber == 0) {
     document.getElementById("weekday").innerHTML = d + "-" + m + "-" + y + " SUNDAY"
     document.getElementById("phrase").innerHTML = "“Your time is limited, so don't waste it living someone else's life.” -Steve Jobs"
@@ -387,7 +326,7 @@ document.oncontextmenu = function(e)
 
 document.getElementById("table1").style.display = "block";
 document.getElementById("table2").style.display = "block";
-document.getElementById("table2").style.display = "block"
+}
 
 
 
@@ -461,11 +400,6 @@ function logined() {
 
 //NOTIFY
 function notify() {
-    notification.body
-    var n = new Date();
-    var hours = n.getHours();
-    var minutes = n.getMinutes();
-    var dayOfWeekNumber = n.getDay();
     var notification = new Notification("CLASS STARTED!", {
         body: "JOIN YOUR CLASS SOON",
     });}
